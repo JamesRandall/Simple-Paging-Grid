23c1a6df2835a82d23558dde251fa624ee1c213f
James Randall <james@accidentalfish.com>
2012-09-08 10:40:50 +0100

    NuGet build files

---
+++ nuget/buildNuGetPackage.ps1
@@ -0,0 +1,9 @@
+# Pick up the CSS and script file
+Remove-Item content\* -Recurse
+New-Item content\Content -ItemType directory
+New-Item content\Scripts -ItemType directory
+Copy-Item ..\css\simplePagingGrid-0.3.css .\content\Content
+Copy-Item ..\script\simplePagingGrid-0.3.js .\content\Scripts
+Copy-Item ..\readme.md .\content\readme.txt
+
+nuget pack .\SimplePagingGrid.nuspec
