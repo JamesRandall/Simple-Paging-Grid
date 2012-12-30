f4ae9064649cbc6fe7ccae0513dc1aa4f107acbf
James Randall <james@accidentalfish.com>
2012-12-30 16:01:16 +0000

    Updated NuGet powershell script

--- nuget/buildNuGetPackage.ps1
+++ nuget/buildNuGetPackage.ps1
@@ -2,8 +2,8 @@
 Remove-Item content\* -Recurse
 New-Item content\Content -ItemType directory
 New-Item content\Scripts -ItemType directory
-Copy-Item ..\css\simplePagingGrid-0.3.css .\content\Content
-Copy-Item ..\script\simplePagingGrid-0.3.js .\content\Scripts
+Copy-Item ..\css\simplePagingGrid-0.4.css .\content\Content
+Copy-Item ..\script\simplePagingGrid-0.4.js .\content\Scripts
 Copy-Item ..\readme.md .\content\readme.txt
 
 nuget pack .\SimplePagingGrid.nuspec
