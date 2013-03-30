79f794a5ea2f869fe7bda7eced11d9112d95551e
James Randall <james@accidentalfish.com>
2013-03-30 20:41:22 +0000

    More unit tests

--- tests/data/data.js
+++ tests/data/data.js
@@ -1,7 +1,6 @@
 function simpleData() {
 	return [
 		{Name: "Apples", Price: 1.50},
-		{Name: "Oranges", Price: 2.25},
-		{Name: "Bananas", Price: 0.75}
+		{Name: "Oranges", Price: 2.25}
 	];
 }
--- tests/unit/tests-0.5.0.2.js
+++ tests/unit/tests-0.5.0.2.js
@@ -45,4 +45,71 @@
 
     	ok(grid.find('.previousContents').length === 0, "previous contents gone");
     });
+
+    test("should have thead element", function () {
+    	var div = $("<div></div>");
+    	var grid = div.simplePagingGrid({data: simpleData()});
+    	var table = grid.children("table");
+    	var thead = table.children("thead");
+
+    	ok (thead.length == 1, "has thead");
+    })
+
+    test("should use default column names from object", function() {
+    	var div = $("<div></div>");
+    	var grid = div.simplePagingGrid({data: simpleData()});
+    	var th = grid.find("th");
+
+    	ok(th.length == 2, "has 2 column headers");
+    	ok($(th.eq(0)).text() === "Name", "has a header called Name");
+    	ok($(th.eq(1)).text() === "Price", "has a header called Price");
+    });
+
+    test("should have a tbody element", function() {
+    	var div = $("<div></div>");
+    	var grid = div.simplePagingGrid({data: simpleData()});
+    	var table = grid.children("table");
+    	var tbody = table.children("tbody");
+
+    	ok (tbody.length == 1, "has tbody");
+    });
+
+    test("should have two rows for simple data", function() {
+    	var div = $("<div></div>");
+    	var grid = div.simplePagingGrid({data: simpleData(), minimumVisibleRows:0});
+    	var table = grid.children("table");
+    	var tbody = table.children("tbody");
+
+    	ok (tbody.children("tr").length == 2, "has two rows");
+    });
+
+    test("should have ten rows due to padding for simple data", function() {
+    	var div = $("<div></div>");
+    	var grid = div.simplePagingGrid({data: simpleData()});
+    	var table = grid.children("table");
+    	var tbody = table.children("tbody");
+
+    	ok (tbody.children("tr").length == 10, "has two rows");
+    });
+
+    test("should have four cells for simple data", function() {
+    	var div = $("<div></div>");
+    	var grid = div.simplePagingGrid({data: simpleData(), minimumVisibleRows:0});
+    	var table = grid.children("table");
+    	var tbody = table.children("tbody");
+
+    	ok (tbody.find("td").length == 4, "has four rows");
+    });
+
+    test("should show data from array", function() {
+    	var div = $("<div></div>");
+    	var grid = div.simplePagingGrid({data: simpleData(), minimumVisibleRows:0});
+    	var table = grid.children("table");
+    	var tbody = table.children("tbody");
+    	var tr0 = tbody.find("tr").eq(0);
+    	var tr1 = tbody.find("tr").eq(1);
+
+    	
+    	ok (tbody.length == 1, "has tbody");
+    });
 })();
